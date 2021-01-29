aa546916ea68ff2f96cbe3e4c18a25f093352bc0
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-01-29 19:46:45 +0600

    add btn

--- src/Navbar/nav.js
+++ src/Navbar/nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Nav, Navbar, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
 
 import logo1 from '../images/logo1.png';
-
+import './nav.css';
 
 function nav() {
     return (
@@ -12,10 +12,11 @@ function nav() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav"  style={{marginTop:"-25px"}}>
     <Nav className="ml-auto">
-    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
-    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
-    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
-    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"22px",fontWeight:"700",lineHeight:"28.13px",marginLeft:"20px"}}>Home</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"22px",fontWeight:"700",lineHeight:"28.13px" ,marginLeft:"20px"}}>Courses</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"22px",fontWeight:"700",lineHeight:"28.13px" ,marginLeft:"20px"}}>Skills</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"22px",fontWeight:"700",lineHeight:"28.13px",marginLeft:"20px",marginRight:"20px"}}>Blogs</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"14px",fontWeight:"700",lineHeight:"16.41px",marginLeft:"20px !important"}} className="m-auto nav-btn">Start Learning</Nav.Link>
     </Nav>
 
   </Navbar.Collapse>
