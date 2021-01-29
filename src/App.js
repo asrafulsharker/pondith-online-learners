c7241e4ab5e7a8aefc394e26a46b48e738aec6e3
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-01-29 23:06:05 +0600

    add11

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Switch , Route} from "react-router-dom";
 import Navbar from './Navbar/nav';
+import ad from './ad/ad';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
 <>
+<Navbar/>
 <Switch>
-  <Route path="/" exact component={Navbar}/>
+  <Route path="/" exact component={ad}/>
 </Switch>
 </>
   );
---
+++ src/ad/ad.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ad() {
+    return (
+        <div style={{background:"rgb(28, 28, 28)",height:"50px"}}>
+            <p style={{textAlign:"center",color:"white"}} className="container m-auto">Pondith - Online Learners’ is an Bangladeshi growing E-learning platform</p>
+        </div>
+    )
+}
+
+export default ad;
