134b8b58cce4ff4a958737a8d66acf284951aa99
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-01-29 20:02:09 +0600

    add5

--- src/Navbar/nav.js
+++ src/Navbar/nav.js
@@ -9,8 +9,8 @@ function nav() {
         <div style={{background:"#1C1C1C",height:"90px",boxShadow:" 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}} >
            <Navbar  expand="lg">
   <Navbar.Brand href="#home"><img style={{height:"100px",width:"244px",marginTop:"-20px"}} src={logo1}/></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav"  style={{marginTop:"-25px"}}>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" style={{background:"white"}}/>
+  <Navbar.Collapse id="basic-navbar-nav"  style={{marginTop:"-5px"}}>
     <Nav className="ml-auto">
     <Nav.Link href="#home" style={{color:"white",fontSize:"22px",fontWeight:"700",lineHeight:"28.13px",marginLeft:"20px"}}>Home</Nav.Link>
     <Nav.Link href="#home" style={{color:"white",fontSize:"22px",fontWeight:"700",lineHeight:"28.13px" ,marginLeft:"20px"}}>Courses</Nav.Link>
