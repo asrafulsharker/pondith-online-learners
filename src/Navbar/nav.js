82d960fab68f59fc2880e92d03ab78f04a3e5afc
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-01-29 18:18:13 +0600

    add4

--- src/Navbar/nav.js
+++ src/Navbar/nav.js
@@ -6,9 +6,9 @@ import logo1 from '../images/logo1.png';
 
 function nav() {
     return (
-        <div style={{background:"#1C1C1C",height:"123px",boxShadow:" 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}} >
+        <div style={{background:"#1C1C1C",height:"90px",boxShadow:" 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}} >
            <Navbar  expand="lg">
-  <Navbar.Brand href="#home"><img style={{height:"138px",width:"244px",marginTop:"-20px"}} src={logo1}/></Navbar.Brand>
+  <Navbar.Brand href="#home"><img style={{height:"100px",width:"244px",marginTop:"-20px"}} src={logo1}/></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav"  style={{marginTop:"-25px"}}>
     <Nav className="ml-auto">
