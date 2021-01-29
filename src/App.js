b7c7e849c5e7abb21fc4e0418c752947730e800e
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-01-29 16:25:57 +0600

    add nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import React from 'react';
 import './App.css';
-
+import {Switch , Route} from "react-router-dom";
+import Navbar from './Navbar/nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-
-    </div>
+<>
+<Switch>
+  <Route path="/" exact component={Navbar}/>
+</Switch>
+</>
   );
 }
 
---
+++ src/Navbar/nav.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button, Nav, Navbar, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
+
+import logo1 from '../images/logo1.png';
+
+
+function nav() {
+    return (
+        <div style={{background:"#1C1C1C",height:"123px",boxShadow:" 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)"}} >
+           <Navbar  expand="lg">
+  <Navbar.Brand href="#home"><img style={{height:"138px",width:"244px",marginTop:"-20px"}} src={logo1}/></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav"  style={{marginTop:"-25px"}}>
+    <Nav className="ml-auto">
+    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
+    <Nav.Link href="#home" style={{color:"white",fontSize:"24px",fontWeight:"700",lineHeight:"28.13px"}}>Home</Nav.Link>
+    </Nav>
+
+  </Navbar.Collapse>
+</Navbar>
+        </div>
+    )
+}
+
+export default nav
