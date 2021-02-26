6a9f455dc22189f67dc27d0a958fa87caf5d3fc5
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-27 00:01:05 +0600

    add

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -16,7 +16,7 @@ export default class MultipleItems extends Component {
   render() {
     const settings = {
       dots: false,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 3
