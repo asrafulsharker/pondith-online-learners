fca0d78c16aa1cddab15ec21ef5b92f096dafb77
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-26 23:32:24 +0600

    add some picture

--- src/platform/platform.js
+++ src/platform/platform.js
@@ -8,7 +8,7 @@ import Ani5 from '../images/ani5.png';
 import './platform.css';
 function platform() {
     return (
-        <div className="hero-platform" style={{paddingBottom:""}}>
+        <div className="hero-platform" style={{paddingBottom:"",backgroundColor:"#1d1d1d",borderTop:"1px solid #4A4848"}}>
                 <img style={{width:"45px",height:"45px"}} className="ani1" src={Ani1}/>
                 <img style={{width:"45px",height:"45px"}}  className="ani3"src={Ani3}/>
                 <img style={{width:"45px",height:"75px"}}   className="ani4"src={Ani4}/>
@@ -18,13 +18,13 @@ function platform() {
                 <div className="row">
                     <div className="col-md-6">
                        <div className="col-md-10">
-                       <img src={PlatformImg}style={{marginTop:"250px",marginBottom:"10px",height:"110%",width:"110%"}}/>
+                       <img src={PlatformImg}style={{marginTop:"250px",marginBottom:"10px",height:"110%",width:"140%"}}/>
                        </div>
                     </div>
                     <div className="col-md-6 ml-auto">
-                        <p className="ml-auto" style={{textAlign:"right",fontWeight:"bold",fontSize:"48px",lineHeight:"46px",paddingTop:"100px"}}>Learn Everything from
+                        <p className="ml-auto" style={{textAlign:"right",fontWeight:"bold",fontSize:"48px",lineHeight:"46px",paddingTop:"100px",color:"white"}}>Learn Everything from
                         One Platform.</p>
-                        <p style={{textAlign:"right",lineHeight:"25px",fontSize:"18px",fontWeight:"bold",paddingTop:"15px"}}>We aim to provide best Academic and skill courses that helps learners, 
+                        <p style={{textAlign:"right",lineHeight:"25px",fontSize:"18px",fontWeight:"bold",paddingTop:"15px",color:"#D7D7D7"}}>We aim to provide best Academic and skill courses that helps learners, 
                         the teachers also can become an instructor in Pondith - Online Learners’ 
                         and can share their knowledges.</p>
                         <div className="mr-auto" style={{    marginLeft: "120px",marginTop: "57px"}}>
