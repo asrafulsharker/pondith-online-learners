ec50fc9e759c9ef07dad38464afb1074820254a7
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-28 22:41:47 +0600

    add some change in why pndith part

--- src/whyPondith/whyPondith.js
+++ src/whyPondith/whyPondith.js
@@ -9,10 +9,10 @@ function whyPondith() {
             <div className="container">
                 <div className="row">
                 <div className="col-md-6">
-                    <img src={WhyPondithImg} style={{width:"90%"}}/>
+                    <img src={WhyPondithImg} style={{width:"95%"}}/>
                 </div>
                 <div className="col-md-5 ml-auto">
-                    <p className="why-top-p">Why Pondith - Online Learners?</p>
+                    <p className="why-top-p"><span style={{color:"black"}}>Why</span> Pondith - Online Learners?</p>
                     <p className="why-bg-p">Pondith - Online Learners’ sets  itself 
                     apart from others.</p>
 
@@ -27,7 +27,7 @@ function whyPondith() {
                         <img className="why-sm-img" src={WhyLearn}/>
                         <div style={{marginLeft:"25px"}}>
                         <p className="why-bg-img-p">Video Playlist to fit your busy life</p>
-                        <p className="why-sm-img-p">Lifetime accessible. <br/>Customizable courses.<br/>Live & Video Playlist courses.</p>
+                        <p className="why-sm-img-p">We teach with real case studies. <br/>Yearly recertification of our experts.<br/>We mentor & Support Trainees.<br/>Extensive multi-industy working Experience.<br/>We teach practically.</p>
                         </div>
                     </div>
                 </div>
