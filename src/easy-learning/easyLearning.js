9c7e6339852e8cb40f214bd2e311f40094f2a1cb
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-03-02 13:07:34 +0600

    add ani

--- src/easy-learning/easyLearning.js
+++ src/easy-learning/easyLearning.js
@@ -3,15 +3,23 @@ import './easyLearning.css';
 import Easy from '../images/easy.png';
 import Earth from '../images/earth.png';
 import Install from '../images/install.png';
+import EasyAni1 from '../images/easy-ani1.png';
+import EasyAni2 from '../images/easy-ani2.png';
+import EasyAni3 from '../images/easy-ani3.png';
+
 function easyLearning() {
     return (
-        <div style={{background:"#FAFAFA"}}>
+        <div style={{backgroundColor:"#FAFAFA",position:"relative"}}>
+                        <img className="easyani1" src={EasyAni1}/>
+                        <img className="easyani2" src={EasyAni2}/>
+                        <img className="easyani3" src={EasyAni3}/>
             <p style={{color:"#161616",textAlign:"center",fontSize:"24px",fontWeight:"bold",padding:"50px 0px 30px 0px",fontFamily:"Russo One"}}>Learning is now more easy</p>
-            <div className="container">
-                <div className="row easy" >
-                    <div className="col-md-6 m-auto" style={{padding:""}}>
+            <div className="container" style={{position:"relative"}} >
+                <div className="row easy" style={{zIndex:"9999"}} >
+
+                    <div className="col-md-6 m-auto" style={{paddingLeft:"60px"}}>
                         <div className="row">
-                            <img src={Earth} style={{height:"51px",height:"65px"}} className="col-md-2"/>
+                            <img src={Earth} style={{width:"48px",height:"63px"}} className="col-md-2"/>
                             <div className="col-md-10">
                                 <p className="easy-bg-p">Visit our website</p>
                                 <p className="easy-sm-p">Now you can start learning each 
@@ -20,7 +28,7 @@ function easyLearning() {
                             </div>
                         </div>
                         <div className="row" style={{paddingTop:"100px"}}>
-                            <img src={Install} style={{height:"51px",height:"65px"}} className="col-md-2"/>
+                            <img src={Install} style={{width:"45px",height:"60px"}} className="col-md-2"/>
                             <div className="col-md-10">
                                 <p className="easy-bg-p">Visit our website</p>
                                 <p className="easy-sm-p">Now you can start learning each 
