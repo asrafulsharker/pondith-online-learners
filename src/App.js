8e048ed35e672389c3f70767de723a8bfccce5da
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-03-08 01:33:21 +0600

    add

--- src/App.js
+++ src/App.js
@@ -9,48 +9,91 @@ import Home from './Home/home';
 import Categories from './categories/categories';
 import {ThemeProvider} from '@material-ui/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Loading from './loading';
 
 
 function App() {
-  const  [loading, setLoading] = useState(false);
-  const container = useRef(null)
 
-  useEffect(()=>{
-      lottie.loadAnimation({
-          container:container.current,
-          renderer:'svg',
-          loop:true,
-          autoplay:true,
-          animationData: require('./ani1.json')
+
+  const [cat, setCat] = useState('');
+  const [loading, setLoading] = useState(undefined);
+  const [done, setDone] = useState(undefined);
+
+  useEffect(() => {
+    getCat();
+  }, []);
+
+  const getCat = () => {
+    setLoading(undefined);
+    setDone(undefined);
+
+    setTimeout(() => {
+      fetch('https://aws.random.cat/meow')
+      .then(res => res.json())
+      .then(data => {
+        setCat(data.file);
+        setLoading(true);
+        setTimeout(() => {
+          setDone(true);
+        }, 1000)
       })
-  },[]);
+      .catch(err => console.log(err));
+    }, 1200);
+  }
 
-  useEffect(()=>{
-    setLoading(true)
-    setTimeout(()=>{
-      setLoading(false)
-    },8000)
-  },[]);
+
+
+  // const  [loading, setLoading] = useState(false);
+  // const container = useRef(null)
+
+  // useEffect(()=>{
+  //     lottie.loadAnimation({
+  //         container:container.current,
+  //         renderer:'svg',
+  //         loop:true,
+  //         autoplay:true,
+  //         animationData: require('./ani1.json')
+  //     })
+  // },[]);
+
+  // useEffect(()=>{
+  //   setLoading(true)
+  //   setTimeout(()=>{
+  //     setLoading(false)
+  //   },11000)
+  // },[]);
 
   return (
-    <>
-        {
-      loading ? ( 
-    <>
-            <div className="" style={{height:"100px"}} ref={container}></div>
-            <p>hi</p>
-    </>
-      ) :(
-        <div>
+    // <>
+    //     {
+    //   loading ? ( 
+    // <div className="App">
+    // {!done?(
+    //   <Loading loading={loading}/>
+    //   ) :(
+    //     <div>
+    //     <Navbar/>
+    //     <Switch>
+    //       <Route path="/" exact component={Home}/>
+    //       <Route path="/auth" exact component={Auth}/>
+    //     </Switch>
+    //     </div>
+    //   )
+    // }
+    // </>
+    <div className="App">
+      {!done?(
+        <Loading loading={loading}/>
+      ):(
+                <div>
         <Navbar/>
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/auth" exact component={Auth}/>
         </Switch>
         </div>
-      )
-    }
-    </>
+      )}
+    </div>
 
 
   );
