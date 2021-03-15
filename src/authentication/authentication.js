eb5c2e08e9f04b84e4cce69e05506a201653b9b8
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-03-15 20:02:07 +0600

    add some changes in want pondith

--- src/authentication/authentication.js
+++ src/authentication/authentication.js
@@ -1,31 +1,10 @@
 import React from 'react';
+import WantPondith from './wantPondith';
+import './authentication.css';
 function authentication() {
     return (
         <div style={{paddingTop:"68px"}}>
-            <div className="authentication">
-            <div className="bg-home" style={{background: "rgba(57, 55, 55, 0.7)",height:"64px",borderBottom:"1px solid #4A4848 "}}>
-            <p style={{textAlign:"center",color:"white",padding:"10px",paddingTop:"20px"}} className="container">Pondith - Online Learners’ is an Bangladeshi growing E-learning platform</p>
-            </div>
-            <div className="container">
-                <p>Want to become a Pondith?</p>
-                <p>We aim to provide best Academic and skill courses that helps learners,  
-                and can share their knowledges.</p>
-                    <div className="col-md-12">
-                    <div className="row " >
-                        <div className=" " style={{height:"4px",background:"#fd08e4",width:"221px",marginLeft:"auto",marginRight:"auto"}}>
-                        </div>
-                        <div className=" " style={{height:"4px",width:"222.5px",background:"#fd08e4",marginLeft:"auto",marginRight:"auto"}}>
-                        </div>
-                        <div className="" style={{height:"4px",width:"222.5px",background:"#fd08e4",marginLeft:"auto",marginRight:"auto"}}>
-                            </div>
-                            <div className=" " style={{height:"4px",width:"222.5px",background:"#fd08e4",marginLeft:"auto",marginRight:"auto"}}>
-                            </div>
-                        <div className=" m-auto" style={{height:"4px",width:"222.5px",background:"#fd08e4"}}>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
+          <WantPondith/>
         </div>
     )
 }
---
+++ src/authentication/wantPondith.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './authentication.css';
+function WantPondith() {
+    return (
+        <div style={{}}>
+            <div className="authentication">
+            <div className="bg-home" style={{background: "rgba(57, 55, 55, 0.7)",height:"64px",borderBottom:"1px solid #4A4848 "}}>
+            <p style={{textAlign:"center",color:"white",padding:"10px",paddingTop:"20px"}} className="container">Pondith - Online Learners’ is an Bangladeshi growing E-learning platform</p>
+            </div>
+            <div className="container">
+                <p className="want-top-text">Want to become a Pondith?</p>
+                <p className="want-top-sm-text">We aim to provide best Academic and skill courses that helps learners,<br/>  
+                and can share their knowledges.</p>
+                    <div className="col-md-12" style={{position:"absolute", marginLeft: "40px"}}>
+                        <div className="text-line1" style={{borderTop:"5px solid #FF0000",width:"199px"}}>
+                            <p style={{paddingTop:"20px"}}>Register</p>
+                            <p className="textLine-sm">We aim to provide best Academic skill 
+                            courses that helps learners,  and can share 
+                            their knowledges.</p>
+                        </div>
+                        <div className="text-line2" style={{borderTop:"5px solid #16F1E4",width:"199px"}}>
+                            <p style={{paddingTop:"20px"}}>Choose Section</p>
+                            <p className="textLine-sm">We aim to provide best Academic skill 
+                            courses that helps learners,  and can share 
+                            their knowledges.</p>
+                        </div> 
+                        <div className="text-line3" style={{borderTop:"5px solid #F8D518",width:"199px"}}>
+                            <p style={{paddingTop:"20px"}}>Make a videolist</p>
+                            <p className="textLine-sm">We aim to provide best Academic skill 
+                            courses that helps learners,  and can share 
+                            their knowledges.</p>
+                        </div>
+                        <div className="text-line4" style={{borderTop:"5px solid #FF4D00",width:"199px"}}>
+                            <p style={{paddingTop:"20px"}}>Upload Playlist</p>
+                            <p className="textLine-sm">We aim to provide best Academic skill 
+                            courses that helps learners,  and can share 
+                            their knowledges.</p>
+                        </div>
+                        <div className="text-line5" style={{borderTop:"5px solid #02F161",width:"199px"}}>
+                            <p style={{paddingTop:"20px"}}>Become a Pondith</p>
+                            <p className="textLine-sm">We aim to provide best Academic skill 
+                            courses that helps learners,  and can share 
+                            their knowledges.</p>
+                        </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default WantPondith;
