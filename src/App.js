88aa6e5b2b166c5922f681cedf813b21ef0bfb38
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-03-28 22:34:25 +0600

    add

--- src/App.js
+++ src/App.js
@@ -50,16 +50,16 @@ function App() {
         <Loading loading={loading}/>
       ):(
       
-          <Router>
+         <Router>
           <Switch>
-          <Route path="/"  component={ver}/>
+          <Route path="/" exact  component={ver}/>
           {/* <Route path="/auth" exact component={Auth}/> */}
-          <Route path="/reg" exact component={reg}/>
-          <Route path="/login" exact component={auth2}/>
+          <Route path="/reg"  component={reg}/>
+          <Route path="/login"  component={auth2}/>
 
         </Switch>
-          </Router>
-
+         
+        </Router>
         
       )}
     </div>
